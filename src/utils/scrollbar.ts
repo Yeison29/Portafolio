ab9d4af805b42edb316cf30f4724402d67186eb8
Yeison Ascanio <ydascanioa@ufpso.edu.co>
2025-05-17 15:56:04 -0500

    ajustando transciciones

--- src/utils/scrollbar.ts
+++ src/utils/scrollbar.ts
@@ -1,8 +1,9 @@
 export const calculateScrollbarWidth = () => {
-    const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
-    document.documentElement.style.setProperty(
-      '--scrollbar-width', 
-      `${scrollbarWidth}px`
-    );
-    return scrollbarWidth;
-  };
+  const scrollbarWidth =
+    window.innerWidth - document.documentElement.clientWidth;
+  document.documentElement.style.setProperty(
+    "--scrollbar-width",
+    `${scrollbarWidth}px`
+  );
+  return scrollbarWidth;
+};
